d932f0d712a6fe403dafa16fdb5ff1455b43fe17
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-10-18 12:31:29 +0100

    Fleshed Out LCA Class

--- LCA.java
+++ LCA.java
@@ -10,5 +10,40 @@ class Node {
 	    }
 	}
 public class LCA {
+		
+		Node root;
+
+		Node findLCA(int q, int w) {
+
+			return findLCA(root, q, w);
+		}
+
+		Node findLCA(Node node, int q, int w) {
+
+			if (node == null) {
+				return null;
+			}
+
+			if (node.data == q || node.data == w) {
+				return node;
+			}
+
+			Node left = findLCA(node.left, q, w);
+			Node right = findLCA(node.right, q, w);
+
+			if (left != null && right != null) {
+				return node;
+			}
+
+			if (left != null) {
+				return left;
+			} else {
+				return right;
+			}
+
+		}
+
+		
+	}
 	
-}
+
--- LCATest.java
+++ LCATest.java
@@ -4,15 +4,4 @@ import org.junit.Test;
 
 public class LCATest {
 		
-		public void test1() {
-			BST test = new BST();
-			test.put(10);
-			test.put(2);
-			test.put(3);
-			test.put(41);
-			test.put(12);
-		}
-		
-	
-
 }
