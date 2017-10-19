1f2c4a2ec2c89b1ce6bfc6b512048a821365eaf4
brandonshark <32677373+brandonshark@users.noreply.github.com>
2017-10-19 17:19:45 +0100

    testNode method

--- LCATest.java
+++ LCATest.java
@@ -3,5 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCATest {
-		
+	@Test
+	public void testNode(){
+		Node one = new Node(1);
+		assertNotNull("testing node: ", one);
+	}
 }
